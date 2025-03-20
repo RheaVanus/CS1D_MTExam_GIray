f6de858b5f20df7e78e5eb8dfe97f19c9756d89e
Jerome Giray <jeromegiray500@gmail.com>
2025-03-20 17:31:32 +0800

    Completed Problem 1

---
+++ 1.js
@@ -0,0 +1,21 @@
+/*
+1. Coding Problem 1: User Information Collection:
+--Create a new JavaScript file named userProfile.js.
+--Collect the following user information with the use of prompt ():
+    Full Name (first and last name)
+    Age
+    Favorite Number
+    Favorite Color
+
+    Use these variables to store the collected information and log it in the console.
+*/
+
+let fullName = prompt("What is your full name?:");
+let Age = parseInt(prompt("How old are you?:"));
+let favNum = parseInt(prompt("What's your favorite number?:"));
+let favColor = prompt("What's your favorite color?:");
+
+console.log(fullName);
+console.log(Age);
+console.log(favNum);
+console.log(favColor);
